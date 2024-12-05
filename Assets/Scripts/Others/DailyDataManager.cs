0cad63ec578f32e8cc6ee6c4899a7fa8d8c6b153
Geul2 <eunsister0614@gmail.com>
2024-12-05 17:00:16 +0900

    여러 계산 함수 추가

--- Assets/Scripts/Others/DailyDataManager.cs
+++ Assets/Scripts/Others/DailyDataManager.cs
@@ -46,7 +46,14 @@ public class DailyDataManager : MonoBehaviour
     //알바 급여
     public int iPartTimerPay = 0;
     //버린 음식으로 내야하는 돈
-    public int WastedFoodCost = 0;
+    public int iWastedFoodCost = 0;
+
+    //이전일 소지금
+    public int iPrevMoney;
+
+    public int iLoan;                   //빚
+
+    public int iDay;                   //몇일차인지 기록
 
     //임시: 스크립트 참조 선언 
     //private OtherScript1 script1;
@@ -110,6 +117,6 @@ public class DailyDataManager : MonoBehaviour
     //    //재료비, 알바 급여, 버린음식 비용 업데이트
     //    iIngredientsCost = script1.GetIngredientsCost();
     //    iPartTimerPay = script2.GetPartTimerPay();
-    //    WastedFoodCost = script3.GetWastedFoodCost();
+    //    iWastedFoodCost = script3.GetWastedFoodCost();
     //}
 }
--- Assets/Scripts/Others/DailyResults.cs
+++ Assets/Scripts/Others/DailyResults.cs
@@ -27,42 +27,71 @@ public class DailyResults : MonoBehaviour
 
 
     //이 아래로 다른 스크립트에서 받아와야함
-    //직장인, 대학생, 교수님, 진상 몇 명 방문했는지 변수
+    public int iOfficerCount = 0;        //직장인 수
+    public int iStudentCount = 0;       //대학생 수
+    public int iProfessorCount = 0;     //교수님 수
+    public int iObnoxiousCount = 0;     //진상 수
 
     //직장인, 대학생, 교수님, 진상 종류별 수익
-
+    public int iOfficerIncome = 0;
+    public int iStudentIncome = 0;
+    public int iProfessorIncome = 0;
+    public int iObnoxiousIncome = 0;
 
     //직장인, 대학생, 교수님, 진상 종류별 만족도 몇 증가 했는가
-
+    public int iOfficerSatisfactionIncrease = 0;
+    public int iStudentSatisfactionIncrease = 0;
+    public int iProfessorSatisfactionIncrease = 0;
+    public int iObnoxiousSatisfactionIncrease = 0;
 
 
     //직장인, 대학생, 교수님, 진상 종류별 만족도 현재 몇 레벨
+    public int iOfficerSatisLevel = 0;
+    public int iStudentSatisLevel = 0;
+    public int iProfessorSatisLevel = 0;
+    public int iObnoxiousSatisLevel = 0;
 
     //직장인, 대학생, 교수님, 진상 종류별 만족도 몇 레벨 증가했는가
+    public int iOfficerSatisUpLv = 0;
+    public int iStudentSatisUpLv = 0;
+    public int iProfessorSatisUpLv = 0;
+    public int iObnoxiousSatisUpLv = 0;
 
     //만족한 손님/불만족한 손님 몇 명
+    public int iSatisfiedCustomers = 0;
+    public int iUnsatisfiedCustomers = 0;
 
     //재료비
-
+    public int iIngredientsCost = 0;
     //알바 급여
-
+    public int iPartTimerPay = 0;
     //버린 음식으로 내야하는 돈
+    public int iWastedFoodCost = 0;
 
+    //이전일 소지금
+    public int iPrevMoney;
+
+
+    public int iLoan;                   //빚
 
     private int iDay;                   //몇일차인지 기록
 
     private int iIncome;                //총 얻은돈
     private int iTotalCost;             //총 지출금
-    private int fExpGet;              //얻은 경험치
-    
-    private int iMoneyToPayLoan;        //현재 대출 상환까지 모아야하는 돈
+    private int fExpGet;                //얻은 경험치
+
     private int iTotalMoney;            //현재 총 소지금(기존 돈 + )
+    private int iMoneyToPayLoan;        //현재 대출 상환까지 모아야하는 돈
+    
+
+    private DailyDataManager dataManager;       //스크립트 참조 선언
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        dataManager = GameObject.FindObjectOfType<DailyDataManager>();
+        setVals();
     }
 
     // Update is called once per frame
@@ -123,6 +152,93 @@ public class DailyResults : MonoBehaviour
     public int getIncome()
     {
         return iIncome;
+    }
+
+    //DailyDataManager에서 각 변수들 가져와서 사용
+    private void setVals()
+    {
+        this.iOfficerCount = dataManager.iOfficerCount;
+        this.iStudentCount = dataManager.iStudentCount;
+        this.iProfessorCount = dataManager.iProfessorCount;
+        this.iObnoxiousCount = dataManager.iObnoxiousCount;
+
+        this.iOfficerIncome = dataManager.iOfficerIncome;
+        this.iStudentIncome = dataManager.iStudentIncome;
+        this.iProfessorIncome = dataManager.iProfessorIncome;
+        this.iObnoxiousIncome = dataManager.iObnoxiousIncome;
+
+        this.iOfficerSatisfactionIncrease = dataManager.iOfficerSatisfactionIncrease;
+        this.iStudentSatisfactionIncrease = dataManager.iStudentSatisfactionIncrease;
+        this.iProfessorSatisfactionIncrease = dataManager.iProfessorSatisfactionIncrease;
+        this.iObnoxiousSatisfactionIncrease = dataManager.iObnoxiousSatisfactionIncrease;
+
+        this.iOfficerSatisLevel = dataManager.iOfficerSatisLevel;
+        this.iStudentSatisLevel = dataManager.iStudentSatisLevel;
+        this.iProfessorSatisLevel = dataManager.iProfessorSatisLevel;
+        this.iObnoxiousSatisLevel = dataManager.iObnoxiousSatisLevel;
+
+        this.iOfficerSatisUpLv = dataManager.iOfficerSatisUpLv;
+        this.iStudentSatisUpLv = dataManager.iStudentSatisUpLv;
+        this.iProfessorSatisUpLv = dataManager.iProfessorSatisUpLv;
+        this.iObnoxiousSatisUpLv = dataManager.iObnoxiousSatisUpLv;
+
+        this.iSatisfiedCustomers = dataManager.iSatisfiedCustomers;
+        this.iUnsatisfiedCustomers = dataManager.iUnsatisfiedCustomers;
+
+        this.iIngredientsCost = dataManager.iIngredientsCost;
+        this.iPartTimerPay = dataManager.iPartTimerPay;
+        this.iWastedFoodCost = dataManager.iWastedFoodCost;
+
+        this.iPrevMoney = dataManager.iPrevMoney;
+        this.iLoan = dataManager.iLoan;
+        this.iDay = dataManager.iDay;
+
+        //수익 계산
+        calIncome();
+        //지출 계산
+        calCost();
+        //경험치 계산
+        calExp();
+        //현재 소지금 계산
+        calMoney();
+        //남은 대출금 계산
+        calLoan();
+    }
+    //수익 계산 함수
+    private void calIncome()
+    {
+        iIncome = iOfficerIncome + iStudentIncome + iProfessorIncome + iObnoxiousIncome;
+    }
+    //지출 계산 함수
+    private void calCost()
+    {
+        iTotalCost = iIngredientsCost + iPartTimerPay + iWastedFoodCost;
+    }
+    //경험치 계산 함수
+    private void calExp()
+    {
+        fExpGet = iOfficerSatisfactionIncrease + iStudentSatisfactionIncrease + iProfessorSatisfactionIncrease + iObnoxiousSatisfactionIncrease;
+    }
+    //현재 소지금 계산 함수
+    private void calMoney()
+    {
+        int iGap = iIncome - iTotalCost;
+        this.iTotalMoney = iPrevMoney + iGap;
+    }
+    //남은 대출금 계산 함수
+    private void calLoan()
+    {
+        this.iMoneyToPayLoan = iLoan - iTotalMoney;
+    }
+
+
+
+    //텍스트 세팅 함수
+
+    //ResultTest1
+    private void setResultText1()
+    {
+
     }
     
 }
